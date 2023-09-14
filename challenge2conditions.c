c871a4af1be7ec6dc3f751ac96fb1ef7d33690bc
cosk <oneno9847@gmail.com>
2023-09-14 11:10:15 +0200

    conditions challenge2

---
+++ challenge2conditions.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+int main()
+{
+    char voyelle;
+    printf("Entre un lettre:");
+    scanf("%c",&voyelle);
+    switch (voyelle)
+    {
+    case 'a':
+        printf("Lettre est un voyelle");
+        break;
+    case 'e':
+        printf("Lettre est un voyelle");
+        break;
+    case 'y':
+        printf("Lettre est un voyelle");
+        break;
+    case 'i':
+        printf("Lettre est un voyelle");
+        break;
+    case 'o':
+        printf("Lettre est un voyelle");
+        break;
+    case 'u':
+        printf("Lettre est un voyelle");
+        break;
+    case 'A':
+        printf("Lettre est un voyelle");
+        break;
+    case 'E':
+        printf("Lettre est un voyelle");
+        break;
+    case 'Y':
+        printf("Lettre est un voyelle");
+        break;
+    case 'I':
+        printf("Lettre est un voyelle");
+        break;
+    case 'O':
+        printf("Lettre est un voyelle");
+        break;
+    case 'U':
+        printf("Lettre est un voyelle");
+        break;
+    default:
+        printf("Lettre est non voyelle");
+        break;
+    }
+}
