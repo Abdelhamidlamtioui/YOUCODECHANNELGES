49d1f60d22bc74ec688195c6700c1a6c4ac9c462
abdelhamidlamtioui <oneno9847@gmail.com>
2023-09-16 01:53:14 +0100

    challenge6

---
+++ challenge6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  float cir,r;
+  float pi=3.14;
+  printf("Entree les rayon:");
+  scanf("%f",&r);
+  cir=2*pi*r;
+  printf("le rayon du cercle est:%d",cir);
+  return 0;
+}
